d1445fe32060b89ebb71226dcdfe37d17af119cb
Lucas R Da Silva <lucasrdasilva@MacBook-Pro-de-Lucas.local>
2026-06-03 18:08:36 -0700

    Correcao test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,9 +1,10 @@
-from fastapi.testclient import TestClient
-import sys
 import os
+os.environ["DATABASE_URL"] = "sqlite:///:memory:"
+import sys
+
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from main import app
-os.environ["DATABASE_URL"] = "sqlite:///:memory:"
+from fastapi.testclient import TestClient
 
 client = TestClient(app)
 
