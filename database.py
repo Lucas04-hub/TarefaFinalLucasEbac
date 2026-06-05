d7251f0b3650942fc44b3ce3654751bf3453ef3c
Lucas R Da Silva <lucasrdasilva@MacBook-Pro-de-Lucas.local>
2026-06-05 09:38:50 -0700

    corrigindo o test_main.py e deletando os pycaches

--- database.py
+++ database.py
@@ -1,6 +1,7 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-import os
+
 
 DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:postgres@db:5432/postgres")
 
